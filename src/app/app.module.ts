1fd9e9911fe7d224c9583aab9ae930bc5b44b413
Yair <ycosio1310@gmail.com>
2018-08-07 23:53:25 -0500

    Responsividad de ambas secciones de features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { NavBarComponent } from './components/navBar/navBar.component';
 import { AppComponent } from './app.component';
+import { FeaturesComponent } from './components/features/features.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule
