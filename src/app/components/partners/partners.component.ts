89e80556f82918027243bc432f4c8d1e92023f2a
Yair <ycosio1310@gmail.com>
2018-08-11 15:35:50 -0500

    Seccion partners terminada

--- src/app/components/partners/partners.component.ts
+++ src/app/components/partners/partners.component.ts
@@ -8,15 +8,21 @@ import { Component } from '@angular/core';
 })
 export class PartnersComponent {
   public slideQuantities:any = {};
+  public foodCostSaving:number = 0;
+  public annualSaving:number = 0;
 
   constructor() {
     this.slideQuantities = {
       ingredientSpending: 36.211,
       timeEmployee: 5
     };
+    this.foodCostSaving = parseFloat((this.slideQuantities.ingredientSpending * 0.3).toFixed(3));
+    this.annualSaving = parseFloat((this.slideQuantities.timeEmployee  * 1337 + this.foodCostSaving).toFixed(3));
   }
 
   public onInputChange(event, key) {
     this.slideQuantities[key] = event.value;
+    this.foodCostSaving = parseFloat((this.slideQuantities.ingredientSpending * 0.3).toFixed(3));
+    this.annualSaving = parseFloat((this.slideQuantities.timeEmployee  * 1337 + this.foodCostSaving).toFixed(3));
   }
 }
